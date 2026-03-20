0a47b98fd61a2f7027bff0d709025f275663e1cb
Kalabrant <paternosterquisincaelis@gmail.com>
2026-03-20 10:08:23 -0400

    Fix: Compilación dinámica con base-href para GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1496620491"
+    serviceWorkerVersion: "3396643757"
   }
 });
